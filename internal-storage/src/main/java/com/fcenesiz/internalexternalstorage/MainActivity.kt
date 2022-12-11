4bdff8abfec0a86cc739f0b1612636297577c0d2
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2022-12-11 18:52:03 +0300

    saveToFile process done

--- internal-storage/src/main/java/com/fcenesiz/internalexternalstorage/MainActivity.kt
+++ internal-storage/src/main/java/com/fcenesiz/internalexternalstorage/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fcenesiz.internalexternalstorage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.fcenesiz.internalexternalstorage.databinding.ActivityMainBinding
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonSaveToFile.setOnClickListener { saveToFile() }
+        binding.buttonInternalStorageFilePath.setOnClickListener { showInternalStorageFilePath() }
+        binding.buttonShowFiles.setOnClickListener { showFiles() }
+        binding.buttonDeleteFileByName.setOnClickListener { deleteFileByName() }
     }
+
+    fun saveToFile(){
+        val filename = binding.editTextTextFilename.text.toString()
+        val data = binding.editTextTextMessage.text.toString()
+
+        var fos : FileOutputStream? = null
+        try {
+            fos = openFileOutput(filename, MODE_PRIVATE)
+            fos.write(data.toByteArray())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }finally {
+            try {
+                fos?.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun showInternalStorageFilePath(){
+
+    }
+
+    fun showFiles(){
+
+    }
+
+    fun deleteFileByName(){
+
+    }
+
 }
